90f55a231314129eb73c469066f314eb886a1f20
wycre <djones@wycre.net>
2025-03-06 14:34:44 -0700

    pract1

---
+++ CodingPractice1/CodingPractice1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CodingPractice1;
+
+class Program
+{
+    
+    // Declare delegate type
+    public delegate int Calculation(int a, int b);
+        
+    // Methods that match the delegate signature
+    public static int Add(int a, int b)
+    {
+        Console.WriteLine(a + b);
+        return a + b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        Console.WriteLine(a * b);
+        return a * b;
+    }
+    
+    static void Main(string[] args)
+    {
+        // Instantiate the delgate and point it to add and multiply
+        Calculation calc = Add;
+
+        calc += Multiply;
+
+        calc(2, 3);
+    }
+}
